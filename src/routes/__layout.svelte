aebcf6ee810265f107f031b72782450d32f3b65a
unsteelix <unsteelix@yandex.ru>
2022-09-13 21:52:19 +0900

    add mobile block

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,16 +1,31 @@
 <script>
     import Footer from '$components/Footer.svelte'
     import Header from '$components/Header.svelte'
+    import { onMount } from 'svelte'
+
+    let isMobile = false
+    onMount(async () => {
+        if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+            isMobile = true
+        }
+    })
 </script>
 
-  
-<Header />
 
-<div class="wrap">
-    <slot></slot>
-</div>
+{#if isMobile}
+    <div class="mobile">
+        Ты что пытался смотреть фотографии с телефона?
+        <img src="facepalm.png" alt="facepalm" />
+        Такое нужно смотреть только с большого дисплея
+    </div>
+{:else}
+    <Header />
+    <div class="wrap">
+        <slot></slot>
+    </div>
+    <Footer />
+{/if}
 
-<Footer />
 
 <style lang="scss">
     @import url('https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600&family=Nunito:ital,wght@0,200;0,300;0,400;0,500;0,600;1,200;1,300;1,400;1,500;1,600&display=swap');
@@ -41,6 +56,24 @@
         align-items: center;
         width: 100%;
         min-height: 100vh;
+    }
+
+    .mobile {
+        display: flex;
+        flex-direction: column;
+        justify-content:space-around;
+        align-items: center;
+        width: 100%;
+        height: 100vh;
+        font-size: 1.4rem;
+        padding: 5rem;
+        text-align: center;
+        
+        img {
+            margin: 20%;
+            width: 50vw;
+            height: auto;
+        }
     }
 
 </style>
